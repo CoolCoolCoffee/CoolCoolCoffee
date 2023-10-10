1e33bca0a42b4128b459d926735bb1ba1feac7ad
ontheway-01 <eunhwa6087@naver.com>
2023-10-10 22:00:59 +0900

    camera_ocr 코드 추가 + 설정

--- CoolCoolCoffee_FrontEnd/test/widget_test.dart
+++ CoolCoolCoffee_FrontEnd/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:coolcoolcoffee_front/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CoolCoolCoffee());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
