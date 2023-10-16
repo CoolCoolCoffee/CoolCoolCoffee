12e3aa9bf6f17cee1ce8d6d3447d9ad07389f549
ontheway-01 <eunhwa6087@naver.com>
2023-10-16 23:21:09 +0900

    메뉴 브랜드 파트 생성 + 뒤로가기

--- CoolCoolCoffee_FrontEnd/lib/menu/menu_page.dart
+++ CoolCoolCoffee_FrontEnd/lib/menu/menu_page.dart
@@ -12,7 +12,54 @@ class MenuPage extends StatefulWidget {
 class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.arrow_back),
+        ),
+        /*bottom: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.star),
+        ),*/
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              IconButton(
+                iconSize: 100.0,
+                  onPressed: () {
+
+                },
+                  icon: Icon(Icons.star)
+              ),
+              IconButton(
+                  iconSize: 100.0,
+                  onPressed: () {
+
+                  },
+                  icon: Icon(Icons.access_alarm)
+              ),
+              IconButton(
+                  iconSize: 100.0,
+                  onPressed: () {
+
+                  },
+                  icon: Icon(Icons.accessible_forward)
+              ),
+              IconButton(
+                  iconSize: 100.0,
+                  onPressed: () {
+
+                  },
+                  icon: Icon(Icons.ac_unit)
+              ),
+            ],
+          ),
+        ),
+      ),
       floatingActionButton: CameraButton(),
     );
   }
