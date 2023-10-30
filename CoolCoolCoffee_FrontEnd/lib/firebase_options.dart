1df2bd91f9f0e1f22ff1acbd91233b78797696f5
ontheway-01 <eunhwa6087@naver.com>
2023-10-30 09:41:22 +0900

    ios camera 연결 확인 (삼설 헬스 연결이 안될 경우를 대비하여..)

--- CoolCoolCoffee_FrontEnd/lib/firebase_options.dart
+++ CoolCoolCoffee_FrontEnd/lib/firebase_options.dart
@@ -26,10 +26,7 @@ class DefaultFirebaseOptions {
       case TargetPlatform.android:
         return android;
       case TargetPlatform.iOS:
-        throw UnsupportedError(
-          'DefaultFirebaseOptions have not been configured for ios - '
-          'you can reconfigure this by running the FlutterFire CLI again.',
-        );
+        return ios;
       case TargetPlatform.macOS:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for macos - '
@@ -59,5 +56,15 @@ class DefaultFirebaseOptions {
     projectId: 'coolcoolcoffee-2a74f',
     databaseURL: 'https://coolcoolcoffee-2a74f-default-rtdb.firebaseio.com',
     storageBucket: 'coolcoolcoffee-2a74f.appspot.com',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyCq2rWyb3-EyyIc8LW62plEhVTwTXV095M',
+    appId: '1:588637140407:ios:080646f359c30bcce6a432',
+    messagingSenderId: '588637140407',
+    projectId: 'coolcoolcoffee-2a74f',
+    databaseURL: 'https://coolcoolcoffee-2a74f-default-rtdb.firebaseio.com',
+    storageBucket: 'coolcoolcoffee-2a74f.appspot.com',
+    iosBundleId: 'com.eunhwa.coolcoolcoffeeFrontend',
   );
 }
