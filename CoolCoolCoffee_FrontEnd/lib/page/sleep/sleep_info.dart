fe168b0bb4d080532c8c8a08940483398fb7e130
hvunrnin <hmkwon0919@cau.ac.kr>
2023-11-04 13:39:18 +0900

    수면 정보 캘린더 페이지로 불러오기 완!!!!!!!!료!!!!!!!!!!!!!

--- CoolCoolCoffee_FrontEnd/lib/page/sleep/sleep_info.dart
+++ CoolCoolCoffee_FrontEnd/lib/page/sleep/sleep_info.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_health_connect/flutter_health_connect.dart';
+import 'package:timezone/timezone.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 class SleepInfoWidget extends StatefulWidget {
   const SleepInfoWidget({Key? key}) : super(key: key);
@@ -8,23 +11,84 @@ class SleepInfoWidget extends StatefulWidget {
 }
 
 class _SleepInfoWidgetState extends State<SleepInfoWidget> {
+  List<HealthConnectDataType> types = [
+    HealthConnectDataType.SleepSession,
+  ];
+  bool readOnly = true;
+  String resultText = '';
+  String resultText_start = '';
+  String resultText_end = '';
+  String resultText_start_real = '';
+  String resultText_end_real = '';
+
+  String token = "";
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white, // Set the background color to brown
+      color: Colors.white,
       child: Center(
         child: Column(
           children: [
-            Text(
-              '어제의 수면정보가 맞나요?',
-              style: TextStyle(
-                fontSize: 15.0,
-                  fontWeight: FontWeight.w500 // Set the text color to white
-              ),
-            ),
-            SizedBox(height: 7), // Add space between text and Row
             Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Adjusted spacing
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '어제의 수면정보가 맞나요?',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () async {
+                    tz.initializeTimeZones();
+                    var startTime = DateTime.now().subtract(const Duration(days: 1));
+                    var endTime = DateTime.now();
+                    try {
+                      final requests = <Future>[];
+                      Map<String, dynamic> typePoints = {};
+                      for (var type in types) {
+                        requests.add(HealthConnectFactory.getRecord(
+                          type: type,
+                          startTime: startTime,
+                          endTime: endTime,
+                        ).then((value) => typePoints.addAll({type.name: value})));
+                      }
+                      await Future.wait(requests);
+                      resultText = '$typePoints';
+
+                      var startTimeEpochSecond_start =
+                      typePoints['SleepSession']['records'][0]['startTime']['epochSecond'];
+                      resultText_start = startTimeEpochSecond_start.toString();
+                      var seoul = getLocation('Asia/Seoul');
+                      resultText_start_real = TZDateTime.fromMillisecondsSinceEpoch(
+                          seoul, startTimeEpochSecond_start * 1000)
+                          .toString();
+
+                      var startTimeEpochSecond_end =
+                      typePoints['SleepSession']['records'][0]['endTime']['epochSecond'];
+                      resultText_end = startTimeEpochSecond_end.toString();
+                      resultText_end_real = TZDateTime.fromMillisecondsSinceEpoch(
+                          seoul, startTimeEpochSecond_end * 1000)
+                          .toString();
+                    } catch (e) {
+                      resultText = e.toString();
+                    }
+                    _updateResultText();
+                    print(resultText_start_real);
+                    print(resultText_end_real);
+
+                    setState(() {});
+                  },
+                  child: const Text('Get Record'),
+                ),
+              ],
+            ),
+            SizedBox(height: 7),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Container(
                   width: 165,
@@ -49,10 +113,10 @@ class _SleepInfoWidgetState extends State<SleepInfoWidget> {
                       ),
                       SizedBox(height: 10),
                       Text(
-                        'AM 1:46',
+                        resultText_start_real,
                         style: TextStyle(
                           color: Colors.black,
-                          fontSize: 25.0,
+                          fontSize: 12.0,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
@@ -82,10 +146,10 @@ class _SleepInfoWidgetState extends State<SleepInfoWidget> {
                       ),
                       SizedBox(height: 10),
                       Text(
-                        'AM 7:16',
+                        resultText_end_real,
                         style: TextStyle(
                           color: Colors.black,
-                          fontSize: 25.0,
+                          fontSize: 12.0,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
@@ -94,9 +158,13 @@ class _SleepInfoWidgetState extends State<SleepInfoWidget> {
                 ),
               ],
             ),
-          ],
+          ], // Closing bracket for Column children
         ),
       ),
     );
+  }
+
+  void _updateResultText() {
+    setState(() {});
   }
 }
