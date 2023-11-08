e70e0d0752e0db2727fe8ac54d97f43cb0ca97d3
hvunrnin <hmkwon0919@cau.ac.kr>
2023-11-08 23:01:54 +0900

    home 화면 구성

--- CoolCoolCoffee_FrontEnd/lib/page/home/home_page.dart
+++ CoolCoolCoffee_FrontEnd/lib/page/home/home_page.dart
@@ -11,8 +11,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color:Colors.cyan,
+    return Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              '홈 화면',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        toolbarHeight: 50,
+        iconTheme: IconThemeData(color: Colors.white),
+    ),
+    body: Text('b'),
     );
   }
 }
