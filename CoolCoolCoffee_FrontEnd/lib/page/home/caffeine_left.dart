45419f2884afb8e9c21b9ccd4e4515c3d7822665
hvunrnin <hmkwon0919@cau.ac.kr>
2023-11-09 12:51:43 +0900

    팝업 창 띄우기

--- CoolCoolCoffee_FrontEnd/lib/page/home/caffeine_left.dart
+++ CoolCoolCoffee_FrontEnd/lib/page/home/caffeine_left.dart
@@ -9,23 +9,98 @@ class CaffeineLeftWidget extends StatefulWidget {
 
 class _CaffeineLeftWidgetState extends State<CaffeineLeftWidget> {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       child: Center(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Container(
-              width: 350,
+              width: 300,
               height: 100,
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: Offset(0, 3), // 그림자 위치 조정
+                  ),
+                ],
               ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '체내 잔여 카페인 양',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Text(
+                    '230/320 (mg)',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _showPopup(context); // 버튼을 누르면 팝업 창 표시
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.brown, // 버튼의 배경색
+                minimumSize: Size(5, 100),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                elevation: 5, // 그림자 효과
+              ),
+              child: Text('그래프'),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void _showPopup(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('카페인 반감기 그래프'),
+          content: Container(
+            width: double.maxFinite,
+            height: 200,
+            color: Colors.brown[100], // Set the color of the rectangular box
+            child: Center(
+              child: Text(
+                '그래프',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the popup
+              },
+              child: Text('닫기'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- CoolCoolCoffee_FrontEnd/lib/page/home/clock.dart
+++ CoolCoolCoffee_FrontEnd/lib/page/home/clock.dart
@@ -12,24 +12,49 @@ class _ClockWidgetState extends State<ClockWidget>{
   @override
   Widget build(BuildContext context){
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      //crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          "카페인 섭취 제한 시작까지",
-          style: TextStyle(
-              color: Colors.black,
-              fontSize: 20
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.baseline,
+          textBaseline: TextBaseline.ideographic,
+          children: [
+            Expanded(
+              child: Text(
+                "카페인 섭취 제한 시작까지",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            SizedBox(width: 0), // Adjust the width as needed
+            ElevatedButton(
+              onPressed: () {
+                _showEditPopup(context); // Show the edit popup
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.brown, // 버튼의 배경색
+                //minimumSize: Size(5, 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                elevation: 5, // 그림자 효과
+              ),
+              child: Text('수정'),
+            ),
+          ],
         ),
         // 두 번째 텍스트 표시
         RichText(
+          textAlign: TextAlign.start,
           text: TextSpan(
             children: [
               TextSpan(
                 text: "n시간 m분",
                 style: TextStyle(
                     color: Colors.orange,
-                    fontSize: 20
+                    fontSize: 20,
                 ),
               ),
               TextSpan(
@@ -52,6 +77,40 @@ class _ClockWidgetState extends State<ClockWidget>{
           ),
         ),
       ],
+    );
+  }
+
+  void _showEditPopup(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('목표 수면 시간'),
+          content: Container(
+            constraints: BoxConstraints(maxHeight: 200), // Set maximum height
+            child: Column(
+              children: [
+                Text('취침 시간'),
+                TextField(
+                  decoration: InputDecoration(labelText: '취침 시간'),
+                ),
+                Text('기상 시간'),
+                TextField(
+                  decoration: InputDecoration(labelText: '기상 시간'),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the popup
+              },
+              child: Text('확인'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
