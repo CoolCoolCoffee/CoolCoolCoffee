20238de153202465ac0ffd60f1bec7357711e0f7
ontheway-01 <eunhwa6087@naver.com>
2023-11-16 15:01:50 +0900

    캘린더 마신 음료 list 띄우기 성공

--- CoolCoolCoffee_FrontEnd/lib/page/sleep/calendar_drink_list.dart
+++ CoolCoolCoffee_FrontEnd/lib/page/sleep/calendar_drink_list.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../model/user_caffeine.dart';
+import '../../service/user_caffeine_service.dart';
+
 class CalendarDrinkListWidget extends StatefulWidget {
   final DateTime selectedDay;
   CalendarDrinkListWidget({required this.selectedDay});
@@ -8,13 +11,16 @@ class CalendarDrinkListWidget extends StatefulWidget {
 }
 
 class _CalendarDrinkListWidgetState extends State<CalendarDrinkListWidget> {
+
   @override
   Widget build(BuildContext context) {
     String selecteddate = widget.selectedDay!.toLocal().toIso8601String().split('T')[0];
+    UserCaffeineService userCaffeineService = UserCaffeineService();
+    userCaffeineService.checkExits(selecteddate);
     return Column(
       children: [
         SizedBox(
-          height: 20,
+          height: 10,
         ),
         Text(
           '${selecteddate}에 마신 음료',
@@ -25,8 +31,8 @@ class _CalendarDrinkListWidgetState extends State<CalendarDrinkListWidget> {
               ),
         ),
         Container(
-          height: 90,
-          width: 330,
+          height: 100,
+          margin: EdgeInsets.symmetric(horizontal: 12),
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(10.0),
@@ -35,7 +41,122 @@ class _CalendarDrinkListWidgetState extends State<CalendarDrinkListWidget> {
               width: 2.0,
             ),
           ),
-          // child: Center(
+          child: StreamBuilder(
+            stream: userCaffeineService.userCaffeineCollection.doc(selecteddate).snapshots(),
+            builder: (context, snapshot){
+              if(snapshot.hasData){
+                var userCaffeineSnapshot= snapshot.data!;
+                if(userCaffeineSnapshot['caffeine_list'].length != 0){
+                 return ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: userCaffeineSnapshot['caffeine_list'].length,
+                  itemBuilder: (context, index) {
+                    var temp = userCaffeineSnapshot['caffeine_list'][index];
+                    UserCaffeine userCaffeine = UserCaffeine(
+                    drinkTime: temp['drink_time'],
+                    menuId: temp['menu_id'],
+                    brand: temp['brand'],
+                    menuImg: temp['menu_img'],
+                    menuSize: temp['menu_size'],
+                    shotAdded: temp['shot_added'],
+                    caffeineContent: temp['caffeine_content']);
+                    return GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                      //color: Colors.blue,
+                        child: Stack(
+                          children: [
+                          FractionallySizedBox(
+                            heightFactor: 1,
+                            child: Container(
+                              width: MediaQuery.of(context).size.width / 4 - 10,
+                              height:  MediaQuery.of(context).size.width / 4 - 10,
+                              margin: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.7),
+                                  spreadRadius: 0,
+                                  blurRadius: 5.0,
+                                  offset: Offset(0, 5))
+                              ],
+                              //color: Colors.green,
+                              image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image:
+                              NetworkImage(userCaffeine.menuImg))),
+                            ),
+                          ),
+                          Align(
+                          alignment: Alignment.bottomCenter,
+                          child: FractionallySizedBox(
+                            heightFactor: 0.3,
+                            child: Container(
+                              width: MediaQuery.of(context).size.width / 4 - 10,
+                              height:  MediaQuery.of(context).size.width / 4 - 10,
+                              alignment: Alignment.bottomLeft,
+                              margin: EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(20)
+                                ),
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.7),
+                                    spreadRadius: 0,
+                                    blurRadius: 5.0,
+                                    offset: Offset(0, 5)
+                                  )
+                                ]
+                              ),
+                              child: Column(
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment:
+                                CrossAxisAlignment.stretch,
+                                children: [
+                                  Text(
+                                    userCaffeine.brand,
+                                    style: TextStyle(
+                                    fontSize: 5, color: Colors.grey),
+                                  ),
+                                  Text(
+                                    userCaffeine.menuId,
+                                    style: TextStyle(
+                                      fontSize: 7,
+                                     ),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                 )
+                                ],
+                              ),
+                             ),
+                            ),
+                         )
+                        ],
+                      ),
+                    ),
+                 );
+               });
+             }else{
+                return Center(
+                  child: Text(
+                    '아직 커피를 안 마셨어요!',
+                    style: TextStyle(
+                      fontSize: 20
+                    ),
+                  ),
+                );
+              }
+            }
+            else{
+              return Text('failed');
+            }
+          },
+        ),
+                  // child: Center(
           //   child: Text(
           //     '${selecteddate}에 마신 음료',
           //     style: TextStyle(
