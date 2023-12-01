19f31f1bd0c2db0b8ecd84531358ce98f0ef03b4
ontheway-01 <eunhwa6087@naver.com>
2023-12-01 22:26:57 +0900

    기기마다 글자 사이즈 통일

--- CoolCoolCoffee_FrontEnd/lib/main.dart
+++ CoolCoolCoffee_FrontEnd/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:coolcoolcoffee_front/page_state/page_state.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:coolcoolcoffee_front/notification/notification_global.dart';
@@ -39,21 +40,26 @@ class CoolCoolCoffee extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-            theme: ThemeData(
-                primarySwatch: Colors.brown,
-            ),
-            debugShowCheckedModeBanner: false,
-            home: StreamBuilder<User?>(
-              stream: FirebaseAuth.instance.authStateChanges(),
-              builder: (ctx, userSnapshot) {
-                if (userSnapshot.hasData && userSnapshot.data != null) {
-                  NotificationGlobal.showDailyNotification();
-                  return PageStates();
-                } else {
-                  return LoginPage();
-                }
-              },
-            ),
+      theme: ThemeData(
+          primarySwatch: Colors.brown,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, userSnapshot) {
+          if (userSnapshot.hasData && userSnapshot.data != null) {
+            NotificationGlobal.showDailyNotification();
+            return PageStates();
+          } else {
+            return LoginPage();
+          }
+        },
+      ),
+      builder: (context,child){
+        return MediaQuery(
+            data: MediaQuery.of(context).copyWith(textScaler: TextScaler.linear(1)),
+            child: child!);
+      },
     );
   }
 }
