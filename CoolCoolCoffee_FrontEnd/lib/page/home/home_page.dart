bc04510b4a756d480e20ed9bb509d075f1e19a45
ontheway-01 <eunhwa6087@naver.com>
2023-12-07 20:55:51 +0900

    알람 회원가입 때 안 뜨게 하기

--- CoolCoolCoffee_FrontEnd/lib/page/home/home_page.dart
+++ CoolCoolCoffee_FrontEnd/lib/page/home/home_page.dart
@@ -49,45 +49,25 @@ class _HomePageState extends ConsumerState<HomePage> {
   void _requestNotificationPermissions() async {
     //알림 권한 요청
     final status = await NotificationGlobal().requestNotificationPermissions();
-    print('허용함??? ${ref.watch(alarmPermissionProvider).isPermissioned} 오늘 이미 뜸?? ${ref.watch(alarmPermissionProvider).isToday}');
-    if(status.isGranted){
+    //print('허용함??? ${ref.watch(alarmPermissionProvider).isPermissioned} 오늘 이미 뜸?? ${ref.watch(alarmPermissionProvider).isToday}');
+    //print(‘허용함??? ${ref.watch(alarmPermissionProvider).isPermissioned} 오늘 이미 뜸?? ${ref.watch(alarmPermissionProvider).isToday}’);
+    if (status.isGranted) {
       ref.watch(alarmPermissionProvider.notifier).setIsPermissioned(true);
       return;
     }
     //오늘 보냈었냐??
-    if (ref.watch(alarmPermissionProvider).isToday) {
+    if (ref
+        .watch(alarmPermissionProvider)
+        .isToday) {
       return;
     }
-    if (status.isDenied && context.mounted) {
+    if (status.isDenied) {
       ref.watch(alarmPermissionProvider.notifier).setIsPermissioned(false);
-      //오늘 이미 보냈다잉
       ref.watch(alarmPermissionProvider.notifier).setPermissionDay(true);
-      showDialog(
-        // 알림 권한이 거부되었을 경우 다이얼로그 출력
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('알림 권한이 거부되었습니다.'),
-          content: Text('알림을 받으려면 앱 설정에서 권한을 허용해야 합니다.'),
-          actions: <Widget>[
-            TextButton(
-              child: Text('설정'), //다이얼로그 버튼의 죄측 텍스트
-              onPressed: () {
-                Navigator.of(context).pop();
-                openAppSettings(); //설정 클릭시 권한설정 화면으로 이동
-              },
-            ),
-            TextButton(
-              child: Text('취소'), //다이얼로그 버튼의 우측 텍스트
-              onPressed: () {
-                Navigator.of(context).pop();
-              }, //다이얼로그 닫기
-            ),
-          ],
-        ),
-      );
+      _showEditPopup(context);
+      //오늘 이미 보냈다잉
     }
   }
-
   Future<void> _initializeSleepParam() async {
     String uid = FirebaseAuth.instance.currentUser!.uid;
     DateTime today = DateTime.now();
@@ -138,6 +118,32 @@ class _HomePageState extends ConsumerState<HomePage> {
     }
   }
 
+  void _showEditPopup(BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('알림 권한이 거부되었습니다.'),
+          content: Text('알림을 받으려면 앱 설정에서 권한을 허용해야 합니다.'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('설정'), //다이얼로그 버튼의 죄측 텍스트
+              onPressed: () {
+                Navigator.of(context).pop();
+                openAppSettings(); //설정 클릭시 권한설정 화면으로 이동
+              },
+            ),
+            TextButton(
+              child: Text('취소'), //다이얼로그 버튼의 우측 텍스트
+              onPressed: () {
+                Navigator.of(context).pop();
+              }, //다이얼로그 닫기
+            ),
+          ],
+        );
+      },
+    );
+  }
   @override
   Widget build(BuildContext context) {
 
